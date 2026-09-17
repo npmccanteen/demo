ad256b50e7f42e8589801cd6d1f9c847388c0150
npmccanteen <anando.siddique0122@gmail.com>
2026-09-17 23:21:09 +0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,4 @@
 // firebase-config.js
-// ⚠️ Public-safe config (client-side এ ব্যবহারের জন্য)
 
 export const firebaseConfig = {
   apiKey: "AIzaSyCmMJ1oQSaespqPdo4Lr8B0d0bXRalPjQA",
@@ -9,3 +8,6 @@ export const firebaseConfig = {
   messagingSenderId: "177224931219",
   appId: "1:177224931219:web:328101a5af09a197adfa66"
 };
+
+// 🔑 এখানে তোর Gemini API Key বসা (Google AI Studio থেকে নেওয়া)
+export const GEMINI_API_KEY = "AQ.Ab8RN6JVuf342wdBXa0osp7y6HeOh2qB1yUX3Xn9vGQUmxlZ7Q";
